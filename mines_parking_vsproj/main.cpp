5421ed26c0b5baaf7b9b5f5be18b53aa3b07758e
Ryan Friedman <rfriedman@mines.edu>
2019-10-25 18:22:30 -0600

    initial code commit

---
+++ mines_parking_vsproj/main.cpp
@@ -0,0 +1,84 @@
+// Main.cpp - Import images
+
+
+
+#include <iostream>
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+#include <windows.h>
+
+using namespace cv;
+using namespace std;
+
+#include "opencv2/highgui/highgui.hpp"
+
+std::vector<std::pair<int, int>> mouse_clicks;
+
+std::enum
+
+
+
+void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+{
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == EVENT_RBUTTONDOWN)
+	{
+		cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == EVENT_MBUTTONDOWN)
+	{
+		cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == EVENT_MOUSEMOVE)
+	{
+		//cout << "Mouse move over the window - position (" << x << ", " << y << ")" << endl;
+
+	}
+}
+
+int main(int argc, char** argv)
+{
+	// Read image from file 
+	Mat img = imread("parking1.png");
+
+	//if fail to read the image
+	if (img.empty())
+	{
+		cout << "Error loading the image" << endl;
+		return -1;
+	}
+
+	//Create a window
+	namedWindow("My Window", 1);
+
+	//set the callback function for any mouse event
+	cv::setMouseCallback("My Window", CallBackFunc, NULL);
+
+	//show the image
+	cv::imshow("My Window", img);
+
+	// Wait until user press some key
+	cv::waitKey(0);
+
+	return 0;
+
+}
